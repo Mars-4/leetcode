6dd098709a9fbae78f3b44efa8f0e0ddba5d0048
Mars-4 <60771062+Mars-4@users.noreply.github.com>
2022-04-16 00:49:50 +0800

    Update 53. 最大子数组和.cpp

--- 53. 最大子数组和.cpp
+++ 53. 最大子数组和.cpp
@@ -20,3 +20,20 @@ public:
         return maxx;
     }
 };
+
+class Solution2 {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int len = nums.size();
+
+        int maxx = INT_MIN;
+        int val = 0;
+        for(int i : nums)
+        {
+            val = max(val + i, i);
+            maxx = max(val, maxx);
+        }
+        return maxx;
+    }
+};//20220416
+
